63766dd27c809e3027600eaa4b66f206b1480c22
SiratSabir78 <siratsabir78@gmail.com>
2024-08-15 21:29:08 +0500

    Chnaged the way signup_screen looks, amtching to that of login screnn

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:roamify/screens/wrapper.dart';
 
-
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
@@ -14,6 +13,7 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+
   signup() async {
     await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email.text, password: password.text);
@@ -23,47 +23,62 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Sign Up Page")),
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+        backgroundColor: const Color.fromARGB(255, 242, 219, 248),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              height: 200,
+              width: 200,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/Roamify.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
             TextField(
               controller: email,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 hintText: 'Enter Email',
-                prefixIcon: Icon(Icons.email),
+                prefixIcon: const Icon(Icons.email),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
+            const SizedBox(height: 20),
             TextField(
               controller: password,
-              decoration: const InputDecoration(
+              obscureText: true,
+              decoration: InputDecoration(
                 hintText: 'Enter Password',
-                prefixIcon: Icon(Icons.lock),
+                prefixIcon: const Icon(Icons.lock),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
-            SizedBox(
-              height: 40.0,
-            ),
+            const SizedBox(height: 40),
             ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  elevation: MaterialStateProperty.all<double>(8.0),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 12.0),
-                  ),
-                  shape: MaterialStateProperty.all<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 12.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                onPressed: (() => signup()),
-                child: const Text("Sign Up")),
+                backgroundColor: const Color.fromARGB(255, 242, 219, 248),
+              ),
+              onPressed: (() => signup()),
+              child: const Text("Sign Up"),
+            ),
           ],
         ),
       ),
