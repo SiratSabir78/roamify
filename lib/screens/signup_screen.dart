740d605cbe96f97f7cd2ee6813c9564fecc52a91
SiratSabir78 <siratsabir78@gmail.com>
2024-08-19 17:21:54 +0500

    Added the functionality of username, and phone number to this page and made some validation errors

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:roamify/screens/login_screen.dart'; // Import LoginPage
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
+import 'package:roamify/screens/login_screen.dart';
+import 'package:roamify/screens/validation_for_signup.dart'; // Import LoginPage
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -13,45 +15,49 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController phoneController =
+      TextEditingController(); // Phone number controller
   final _formKey = GlobalKey<FormState>();
 
   Future<void> signup() async {
     if (_formKey.currentState!.validate()) {
       try {
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        // Create user with email and password
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: emailController.text.trim(),
           password: passwordController.text.trim(),
         );
-        // Navigate to LoginPage instead of Wrapper
+
+        // Store additional user information in Firestore
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'username': usernameController.text.trim(),
+          'email': emailController.text.trim(),
+          'profilePicture': '', // Initially empty
+          'reviews': [], // Initially empty list
+          'favoriteCities': [], // Initially empty list
+          'bookmarkedCities': [], // Initially empty list
+          'phoneNumber': phoneController.text.trim(), // Store phone number
+          'travelHistory': [], // Initially empty list
+          'userId': userCredential.user!.uid,
+        });
+
+        // Navigate to LoginPage
         Get.offAll(() => const LoginPage());
       } catch (e) {
         Get.snackbar(
           'Sign Up Error',
           e.toString(),
           snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.red,
+          backgroundColor: Color.fromARGB(255, 221, 128, 244),
           colorText: Colors.white,
         );
       }
     }
-  }
-
-  String? validateEmail(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter your email';
-    } else if (!value.endsWith('@gmail.com')) {
-      return 'Email must end with @gmail.com';
-    }
-    return null;
-  }
-
-  String? validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter your password';
-    } else if (value.length < 6) {
-      return 'Password must be at least 6 characters long';
-    }
-    return null;
   }
 
   @override
@@ -62,78 +68,124 @@ class _SignUpState extends State<SignUp> {
         backgroundColor: const Color.fromARGB(255, 242, 219, 248),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("images/Roamify.png"),
-                  fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 200,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("images/Roamify.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: usernameController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Username',
+                          prefixIcon: const Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email',
+                          prefixIcon: const Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          String? error = validateEmail(value);
+                          
+                          return error;
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        controller: phoneController,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Phone Number',
+                          prefixIcon: const Icon(Icons.phone),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          String? error = validatePhoneNumber(value);
+                          
+                          return error;
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password',
+                          prefixIcon: const Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          String? error = validatePassword(value);
+                          
+                          return error;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 12.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          backgroundColor:
+                              const Color.fromARGB(255, 242, 219, 248),
+                        ),
+                        onPressed: signup,
+                        child: const Text("Sign Up"),
+                      ),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () => Get.to(() => const LoginPage()),
+                        child: const Text(
+                          "Already have an account? Login",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 221, 128, 244)),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Enter Email',
-                      prefixIcon: const Icon(Icons.email),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    ),
-                    validator: validateEmail,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Enter Password',
-                      prefixIcon: const Icon(Icons.lock),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    ),
-                    validator: validatePassword,
-                  ),
-                  const SizedBox(height: 40),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      backgroundColor: const Color.fromARGB(255, 242, 219, 248),
-                    ),
-                    onPressed: signup,
-                    child: const Text("Sign Up"),
-                  ),
-                  const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: () => Get.to(() => const LoginPage()),
-                    child: const Text(
-                      "Already have an account? Login",
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 221, 128, 244)),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
