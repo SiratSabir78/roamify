aef4cf36a1afed38b31e7120e3bca6471e873d95
SiratSabir78 <siratsabir78@gmail.com>
2024-08-21 19:42:57 +0500

    Some changes in validation for phone number and username

--- lib/screens/validation_for_signup.dart
+++ lib/screens/validation_for_signup.dart
@@ -1,3 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+Future<String?> validateUsername(String? value) async {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your username';
+  }
+
+  // Check if the username already exists by querying the Firestore collection
+  final result = await FirebaseFirestore.instance
+      .collection('users')
+      .where('username', isEqualTo: value)
+      .limit(1)
+      .get();
+
+  if (result.docs.isNotEmpty) {
+    return 'Username already taken';
+  }
+
+  return null; 
+}
+
+
+
+
 String? validateEmail(String? value) {
   if (value == null || value.isEmpty) {
     return 'Please enter your email';
@@ -10,8 +33,10 @@ String? validateEmail(String? value) {
 String? validatePassword(String? value) {
   if (value == null || value.isEmpty) {
     return 'Please enter your password';
-  } else if (value.length < 6) {
-    return 'Password must be at least 6 characters long';
+  } else if (value.length < 7) {
+    return 'Password must be at least 7 characters long';
+  } else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
+    return 'Password is weak, please include at least one special character';
   }
   return null;
 }
@@ -21,10 +46,8 @@ String? validatePhoneNumber(String? value) {
     return 'Please enter your phone number';
   } else if (!RegExp(r'^\d+$').hasMatch(value)) {
     return 'Phone number must be numeric';
-  } else if (value.length > 12) {
-    return 'Phone number must not exceed 12 digits';
-  } else if (value.length < 12) {
-    return 'Phone number must have at least 12 digits';
+  } else if (value.length != 11 || !value.startsWith('03')) {
+    return 'Enter a valid Pakistani phone number (e.g., 03XXXXXXXXX)';
   }
   return null;
 }
