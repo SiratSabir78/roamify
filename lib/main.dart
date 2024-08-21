632970a202f7753b3c4b7321a39cb553cc36ad2a
SiratSabir78 <siratsabir78@gmail.com>
2024-08-21 23:53:14 +0500

    Simple main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:roamify/screens/favorites_provider.dart';
+import 'package:roamify/screens/favorites_screen.dart';
 import 'package:roamify/screens/wrapper.dart';
 import 'firebase_options.dart';
 
