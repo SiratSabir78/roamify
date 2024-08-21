6a4d63950859c08137c535afa585c90bd6eede57
SiratSabir78 <siratsabir78@gmail.com>
2024-08-21 23:53:52 +0500

    post screen has write a review and add to favourite option

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:roamify/screens/post_bottom_bar.dart';
 import 'package:roamify/screens/review_screens.dart';
 
@@ -25,8 +27,7 @@ class PostScreen extends StatelessWidget {
         children: [
           Column(
             children: [
-              Image.network(
-                // Use network image instead of asset
+              Image.asset(
                 imagePath,
                 height: MediaQuery.of(context).size.height / 2,
                 width: double.infinity,
@@ -49,12 +50,27 @@ class PostScreen extends StatelessWidget {
                       builder: (context) => ReviewPage(
                         cityId: cityId,
                         cityName: cityName,
-                        cityImageUrl: imagePath,
+                        //cityImageUrl: imagePath,
                       ),
                     ),
                   );
                 },
                 child: const Text('Write a Review'),
+              ),
+              const SizedBox(height: 20), // Add spacing
+              ElevatedButton(
+                onPressed: () async {
+                  await _addToFavorites(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 12.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 242, 219, 248),
+                ),
+                child: const Text('Add to Favorites'),
               ),
               const SizedBox(height: 20), // Add spacing
               Expanded(
@@ -68,5 +84,66 @@ class PostScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _addToFavorites(BuildContext context) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('You need to be logged in to add favorites'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      return;
+    }
+
+    final userId = user.uid;
+    final userDoc = FirebaseFirestore.instance.collection('users').doc(userId);
+
+    try {
+      final userDocSnapshot = await userDoc.get();
+      if (!userDocSnapshot.exists) {
+        // Handle case where user document doesn't exist
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('User data not found.'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        return;
+      }
+
+      final userData = userDocSnapshot.data()!;
+      final List<dynamic> favorites = userData['favoriteCities'] ?? [];
+
+      if (favorites.contains(cityName)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('City already in favorites.'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        return;
+      }
+
+      await userDoc.update({
+        'favoriteCities': FieldValue.arrayUnion([cityName])
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Added to your favorites'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error adding to favorites'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
   }
 }
