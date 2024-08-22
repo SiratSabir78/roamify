f2b11374940c593421b7bf31ab2471526c625d88
SiratSabir78 <siratsabir78@gmail.com>
2024-08-22 22:09:05 +0500

    Changes for the username and password

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:roamify/screens/user_profile_screen/change_password_screen.dart';
-import 'package:roamify/screens/user_profile_screen/change_username_screen.dart';
+import 'package:flutter/widgets.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -32,39 +31,183 @@ class _ProfileScreenState extends State<ProfileScreen> {
     }
   }
 
-  void _navigateToChangeUsernameScreen() async {
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => ChangeUsernameScreen()),
-    );
-    _loadUsername(); // Refresh username after returning from ChangeUsernameScreen
-  }
+  void _showChangeUsernameDialog() {
+    TextEditingController usernameController = TextEditingController();
+    String? usernameError;
 
-  void _navigateToChangePasswordScreen() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => ChangePasswordScreen()),
-    );
-  }
-
-  void _showOptionsMenu(BuildContext context, String title, Widget screen) {
-    showModalBottomSheet(
+    showDialog(
       context: context,
-      builder: (BuildContext context) {
-        return Wrap(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.edit, color: Theme.of(context).primaryColor),
-              title: Text(title),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => screen),
-                );
-              },
-            ),
-          ],
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Change Username'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'New Username',
+                      errorText: usernameError,
+                    ),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    if (_user != null) {
+                      String newUsername = usernameController.text.trim();
+
+                      if (newUsername == _username) {
+                        setState(() {
+                          usernameError =
+                              'Username is the same as the current one. Please choose a different username.';
+                        });
+                      } else if (newUsername.isEmpty) {
+                        setState(() {
+                          usernameError = 'Username cannot be empty.';
+                        });
+                      } else {
+                        try {
+                          await FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(_user!.uid)
+                              .update({'username': newUsername});
+                          _loadUsername(); // Refresh the username on the profile screen
+                          Navigator.pop(context);
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Username changed successfully!')));
+                        } catch (e) {
+                          setState(() {
+                            usernameError = 'Failed to change username: $e';
+                          });
+                        }
+                      }
+                    }
+                  },
+                  child: Text('Save'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _showChangePasswordDialog() {
+    TextEditingController currentPasswordController = TextEditingController();
+    TextEditingController newPasswordController = TextEditingController();
+    TextEditingController confirmPasswordController = TextEditingController();
+
+    String? currentPasswordError;
+    String? newPasswordError;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Change Password'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: currentPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'Current Password',
+                      errorText: currentPasswordError,
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 8),
+                  TextField(
+                    controller: newPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'New Password',
+                      errorText: newPasswordError,
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      if (value == confirmPasswordController.text) {
+                        setState(() {
+                          newPasswordError = null;
+                        });
+                      } else {
+                        setState(() {
+                          newPasswordError = 'Passwords do not match';
+                        });
+                      }
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  TextField(
+                    controller: confirmPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm New Password',
+                      errorText: newPasswordError,
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      if (value == newPasswordController.text) {
+                        setState(() {
+                          newPasswordError = null;
+                        });
+                      } else {
+                        setState(() {
+                          newPasswordError = 'Passwords do not match';
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    if (newPasswordController.text !=
+                        confirmPasswordController.text) {
+                      setState(() {
+                        newPasswordError = 'Passwords do not match';
+                      });
+                      return;
+                    }
+
+                    try {
+                      AuthCredential credential = EmailAuthProvider.credential(
+                          email: _user!.email!,
+                          password: currentPasswordController.text);
+                      await _user!.reauthenticateWithCredential(credential);
+                      await _user!.updatePassword(newPasswordController.text);
+                      Navigator.pop(context);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('Password changed successfully!')));
+                    } catch (e) {
+                      setState(() {
+                        currentPasswordError = 'Incorrect current password';
+                      });
+                    }
+                  },
+                  child: Text('Save'),
+                ),
+              ],
+            );
+          },
         );
       },
     );
@@ -101,7 +244,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     },
                     child: Text(
                       'Change Profile Picture',
-                      style: TextStyle(color: Color.fromARGB(255, 221, 128, 244)),
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 221, 128, 244)),
                     ),
                   ),
                 ],
@@ -112,20 +256,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
             // Username Tile
             if (_user != null)
               ListTile(
-                title: Text('Username', style: TextStyle(color: textColor)),
-                subtitle: Text(_username, style: TextStyle(color: textColor)),
-                trailing: Icon(Icons.more_vert, color: primaryColor),
-                onTap: _navigateToChangeUsernameScreen,
+                title: Row(
+                  children: [
+                    Text('Username: ', style: TextStyle(color: textColor)),
+                    Text(_username, style: TextStyle(color: primaryColor)),
+                  ],
+                ),
+                trailing: Icon(Icons.edit, color: primaryColor),
+                onTap: _showChangeUsernameDialog,
               ),
             Divider(color: Colors.grey),
 
             // Password Tile
             if (_user != null)
               ListTile(
-                title: Text('Password', style: TextStyle(color: textColor)),
-                subtitle: Text('********', style: TextStyle(color: textColor)),
-                trailing: Icon(Icons.more_vert, color: primaryColor),
-                onTap: _navigateToChangePasswordScreen,
+                title: Row(
+                  children: [
+                    Text('Password: ', style: TextStyle(color: textColor)),
+                    Text('********', style: TextStyle(color: primaryColor)),
+                  ],
+                ),
+                trailing: Icon(Icons.edit, color: primaryColor),
+                onTap: _showChangePasswordDialog,
               ),
             Divider(color: Colors.grey),
           ],
