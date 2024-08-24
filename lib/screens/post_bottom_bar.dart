4c0fb47498e5a24d0d457e5c699545783117ed56
SiratSabir78 <siratsabir78@gmail.com>
2024-08-24 21:22:22 +0500

    made the escription adn the city name dynamic

--- lib/screens/post_bottom_bar.dart
+++ lib/screens/post_bottom_bar.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class PostBottomBar extends StatelessWidget {
+  final String cityName;
+
+  final String description;
+
+  PostBottomBar({
+    required this.cityName,
+    required this.description,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,88 +33,19 @@ class PostBottomBar extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "City Name, Country",
+                      "$cityName, Pakistan",
                       style: TextStyle(
                         fontSize: 23,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                          size: 25,
-                        ),
-                        Text(
-                          "4.5",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
                 SizedBox(height: 25),
                 Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Gravida neque convallis a cras semper auctor neque vitae. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum.",
+                  description,
                   style: TextStyle(color: Colors.black54, fontSize: 16),
                   textAlign: TextAlign.justify,
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 5),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          "images/city5.jpeg",
-                          fit: BoxFit.cover,
-                          width: 120,
-                          height: 90,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 5),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          "images/city4.jpeg",
-                          fit: BoxFit.cover,
-                          width: 120,
-                          height: 90,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 120,
-                        height: 90,
-                        margin: EdgeInsets.only(right: 5),
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(15),
-                          image: DecorationImage(
-                            image: AssetImage("images/city6.jpeg"),
-                            fit: BoxFit.cover,
-                            opacity: 0.4,
-                          ),
-                        ),
-                        child: Text(
-                          "10+",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
                 SizedBox(height: 15),
                 Container(
@@ -114,20 +53,6 @@ class PostBottomBar extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black26,
-                              blurRadius: 4,
-                            ),
-                          ],
-                        ),
-                        child: Icon(Icons.bookmark_outline, size: 40),
-                      ),
                       Container(
                         padding:
                             EdgeInsets.symmetric(vertical: 15, horizontal: 25),
@@ -147,6 +72,7 @@ class PostBottomBar extends StatelessWidget {
                             color: Colors.white,
                             fontSize: 26,
                             fontWeight: FontWeight.w500,
+                            
                           ),
                         ),
                       ),
